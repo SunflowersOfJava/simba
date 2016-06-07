15cd3eab22e3b42040730e1efabc637235904457
caozhejun <loceme.student@163.com>
2016-06-07 09:43:56 +0800

    扩展用户属性如果_r结尾，则为必填

--- src/main/java/com/caozj/test/TestInitService.java
+++ src/main/java/com/caozj/test/TestInitService.java
@@ -7,11 +7,7 @@ import javax.annotation.PostConstruct;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.caozj.dao.permission.OrgDao;
-import com.caozj.model.permission.Org;
 
 /**
  * 测试使用
@@ -24,13 +20,7 @@ public class TestInitService {
 
 	private static final Log logger = LogFactory.getLog(TestInitService.class);
 
-	@Autowired
-	private OrgDao orgDao;
-
 	@PostConstruct
 	private void init() throws IOException, URISyntaxException {
-		Org org = new Org();
-		org.setName("org");
-		logger.info("================================new org and its id is " + orgDao.add(org));
 	}
 }
