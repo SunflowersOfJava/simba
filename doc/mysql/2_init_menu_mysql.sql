29fba985bec6d1f2f59a71394219e829374bf82a
caozhejun <loceme.student@163.com>
2016-06-23 11:13:26 +0800

    工作流

--- doc/mysql/2_init_menu_mysql.sql
+++ doc/mysql/2_init_menu_mysql.sql
@@ -7,3 +7,12 @@ insert into menu(text,parentID,url,orderNo) values('业务管理',-1,'/buss/list.do'
 insert into menu(text,parentID,url,orderNo) values('版本管理',-1,'/install/list.do',7);
 insert into menu(text,parentID,url,orderNo) values('注册类型管理',-1,'/registryType/list.do',8);
 insert into menu(text,parentID,url,orderNo) values('注册表管理',-1,'/registryTable/list.do',9);
+insert into menu(text,parentID,url,orderNo) values('流程管理',-1,'/processManagement/list.do',10);
+insert into menu(text,parentID,url,orderNo) values('待办任务',-1,'/processDoing/list.do',11);
+insert into menu(text,parentID,url,orderNo) values('已办任务',-1,'/processDone/list.do',12);
+insert into menu(text,parentID,url,orderNo) values('启动流程',-1,'/processStart/list.do',13);
+insert into menu(text,parentID,url,orderNo) values('流程监控',-1,'/processMonitor/list.do',14);
+insert into menu(text,parentID,url,orderNo) values('代办设置',-1,'/processAgencySet/list.do',15);
+insert into menu(text,parentID,url,orderNo) values('已归档流程',-1,'/processFinish/list.do',16);
+insert into menu(text,parentID,url,orderNo) values('运行中流程',-1,'/processRunning/list.do',17);
+insert into menu(text,parentID,url,orderNo) values('作业管理',-1,'/processJob/list.do',18);
