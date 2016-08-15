e766e46a117da54aa60e60d61bcb3d8d1f2d4b17
caozhejun <loceme.student@163.com>
2016-08-15 17:30:31 +0800

    SpringBatch

--- src/main/java/com/caozj/test/TestInitService.java
+++ src/main/java/com/caozj/test/TestInitService.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -36,8 +37,10 @@ public class TestInitService {
   @PostConstruct
   private void init() throws IOException, URISyntaxException, JobExecutionAlreadyRunningException,
       JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
-    JobExecution result = jobLauncher.run(testJob, new JobParameters());
+    JobExecution result = jobLauncher.run(testJob, new JobParametersBuilder().addLong("date", System.currentTimeMillis()).toJobParameters());
+    System.out.println("******1****************" + result.toString());
+    JobExecution result2 = jobLauncher.run(testJob, new JobParametersBuilder().addLong("date", System.currentTimeMillis()).toJobParameters());
     // /* 处理结束，控制台打印处理结果 */
-    System.out.println("**********************" + result.toString());
+    System.out.println("*******2***************" + result2.toString());
   }
 }
