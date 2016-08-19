829476848d6dc70b49d831f671e20b83cbac0c14
caozhejun <loceme.student@163.com>
2016-08-19 14:54:35 +0800

    SpringBatch

--- src/main/java/com/caozj/test/T.java
+++ src/main/java/com/caozj/test/T.java
@@ -1,10 +1,9 @@
 package com.caozj.test;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
-import com.caozj.framework.util.freemarker.FreemarkerUtil;
+import com.caozj.framework.util.file.PropertiesUtil;
 
 import freemarker.core.ParseException;
 import freemarker.template.MalformedTemplateNameException;
@@ -12,13 +11,16 @@ import freemarker.template.TemplateException;
 import freemarker.template.TemplateNotFoundException;
 
 public class T {
-	
-	public static void main(String[] args) throws TemplateNotFoundException, MalformedTemplateNameException, ParseException, IOException, TemplateException {
-		Map<String,Object> param = new HashMap<>();
-		param.put("a", "我是你大哥");
-		param.put("pageType", "treeTable");
-		String content = "${a},大哥您好啊！！！over!!!<#if pageType!=\"treeTable\">text</#if>";
-		System.out.println(FreemarkerUtil.parseContent(content, param));
-	}
-	
+
+  public static void main(String[] args) throws TemplateNotFoundException,
+      MalformedTemplateNameException, ParseException, IOException, TemplateException {
+    Map<String, String> p = PropertiesUtil.readFileByOrder(
+        "D:\\local\\develop2.0\\SmartAs\\target\\depoly\\xxx-web\\src\\main\\resources\\config\\app.properties");
+    p.forEach((key,value)->{
+      System.out.println(key+":"+value);
+    });
+    p.put("datasource.driverClassName", "com.mysql.jdbc.Driver-abcde");
+    PropertiesUtil.saveFileByOrder("D:\\local\\develop2.0\\SmartAs\\target\\depoly\\xxx-web\\src\\main\\resources\\config\\app.properties", p);
+  }
+
 }
