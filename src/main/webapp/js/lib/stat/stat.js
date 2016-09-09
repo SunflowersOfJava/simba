122a3a745520d932a4d10552ee11a17a17bd07e6
caozhejun <loceme.student@163.com>
2016-09-09 14:31:13 +0800

    统计js

--- src/main/webapp/js/lib/stat/stat.js
+++ src/main/webapp/js/lib/stat/stat.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?ee7b4a007ffc3a189a48411c9cc18b03";
+  hm.src = "//hm.baidu.com/hm.js?df08cbb7a12c0c144371519617a38881";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
