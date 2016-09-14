25a96b176efdd6a109cff5f5c847cb4cfa7c5666
caozhejun <loceme.student@163.com>
2016-09-14 17:59:08 +0800

    用sigar获取服务器相关性能数据

---
+++ src/main/java/com/caozj/framework/util/sigar/SigarUtil.java
@@ -0,0 +1,44 @@
+package com.caozj.framework.util.sigar;
+
+import com.caozj.framework.util.common.ServerUtil;
+import com.caozj.framework.util.common.SystemUtil;
+
+/**
+ * 利用sigar获取系统性能相关信息工具类(memory,cpu,disk,io network等)
+ * 
+ * @author caozj
+ *
+ */
+public class SigarUtil {
+
+  private SigarUtil() {
+    init();
+  }
+
+  /**
+   * 初始化
+   */
+  private void init() {
+    // 处理java.library.path路径，保证sigar能够找到对应操作系统的库文件
+    String sigarFile = ServerUtil.getWebRoot() + "/sigar";
+    String path = SystemUtil.getSystemEnvPath();
+    // 为防止java.library.path重复加，此处判断了一下
+    if (!path.contains(sigarFile)) {
+      if (SystemUtil.isWindowsOs()) {
+        path += ";" + sigarFile;
+      } else {
+        path += ":" + sigarFile;
+      }
+      System.setProperty("java.library.path", path);
+    }
+  }
+
+  private static final class SigarUtilHolder {
+    private static SigarUtil instance = new SigarUtil();
+  }
+
+  public static SigarUtil getInstance() {
+    return SigarUtilHolder.instance;
+  }
+
+}
---
+++ src/main/java/com/caozj/test/TOS.java
@@ -0,0 +1,12 @@
+package com.caozj.test;
+
+import com.caozj.framework.util.common.SystemUtil;
+
+public class TOS {
+
+  public static void main(String[] args) {
+    System.out.println(SystemUtil.getOsName());
+    System.out.println(SystemUtil.isWindowsOs());
+  }
+
+}
