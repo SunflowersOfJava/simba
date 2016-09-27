814af07b06c0ba9972d336c50b109e4f5823f30a
caozhejun <loceme.student@163.com>
2016-09-27 11:46:22 +0800

    复杂的word操作工具类

---
+++ src/main/java/com/caozj/framework/util/office/WordUtil.java
@@ -0,0 +1,11 @@
+package com.caozj.framework.util.office;
+
+/**
+ * 复杂的word操作工具类(包括使用freemark语法解析word，插入表格，插入图表，插入图片等)
+ * 
+ * @author caozj
+ *
+ */
+public class WordUtil {
+
+}
---
+++ src/main/java/com/caozj/test/TestWord.java
@@ -0,0 +1,11 @@
+package com.caozj.test;
+
+public class TestWord {
+
+  //table \ charts
+  public static void main(String[] args) {
+    String wordFile = "d://test.docx";
+
+  }
+
+}
